0d196b4010ef932053b23178280a6a2f9f774bc0
Yasser Sharaf <yasser.sharaf@gmail.com>
2023-08-29 14:24:48 +0300

    Update remote-video.js

--- web-admin/js/remote-video.js
+++ web-admin/js/remote-video.js
@@ -12,11 +12,13 @@ function RemoteVideo(remoteVideoElem, videoLoader, videoStats) {
     var obj = this;  // for event handlers
 
     this.getStreamVideotracks = function(){
+        console.log('stream', this.stream);
         return this.stream ? this.stream.getVideoTracks() : [];
     }
 
     this.noRemoteVideo = function () {
         if (!this.isVideoAlreadyPlayed || window.debugUtils.isDebugEnabled()) {
+            console.log('no video: videoLoader.show');
             this.videoLoader.show();
         }
         console.debug('video: no remote');
@@ -39,19 +41,23 @@ function RemoteVideo(remoteVideoElem, videoLoader, videoStats) {
     this.setStream = function (stream) {
         let streamChanged = false;
         if (this.stream !== stream) {
+            console.log('this.stream !== stream');
             this.stream = stream;
             streamChanged = true;
         }
-
+console.log('this.getStreamVideotracks().length: ' + this.getStreamVideotracks().length);
         if (this.getStreamVideotracks().length > 0) {
             if (streamChanged) {
+                console.log('streamChanged');
                 Janus.attachMediaStream(this.remoteVideoElem.get(0), this.stream);
             }
             this.hasRemoteVideo();
             if (['chrome', 'firefox', 'safari'].indexOf(Janus.webRTCAdapter.browserDetails.browser) >= 0) {
+                console.log('this.videoStats.start');
                 this.videoStats.start();
             }
         } else {
+            console.log('noRemoteVideo');
             this.noRemoteVideo();
             this.videoStats.stop();
         }
@@ -62,19 +68,23 @@ function RemoteVideo(remoteVideoElem, videoLoader, videoStats) {
         console.info("streaming: starting mountpoint id " + mountpointId + ' with pin ' + pin);
 
         var body = {"request": "watch", "id": mountpointId, "pin": pin};
+        console.log("message: "+ body);
         this.streaming.send({"message": body});
         this.noRemoteVideo();
     }
 
     this.remoteVideoElem.on("playing", function (e) {
+        comsole.log('remoteVideoElem on playing');
         console.debug('video: playing event', e);
 
         if (obj.getStreamVideotracks().length > 0) {
+            console.log('obj.getStreamVideotracks().length: ' + obj.getStreamVideotracks().length);
             obj.videoStats.start();
             remoteVideoElem = obj.remoteVideoElem.get(0);
             obj.setResolution(remoteVideoElem.videoWidth, remoteVideoElem.videoHeight);
             obj.isVideoAlreadyPlayed = true;
         } else {
+            console.log('obj.videoStats.stop');
             obj.videoStats.stop();
         }
     });
